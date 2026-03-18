ab6a86e296452eaccbb50e12440b6eae8c06c983
lorenasandoval88 <43822820+lorenasandoval88@users.noreply.github.com>
2026-03-18 15:25:54 -0400

    export localforage

--- sdk.js
+++ sdk.js
@@ -3,6 +3,7 @@ import localforage from "localforage";
 import { displayProfiles } from "./src/js/get23_loadProfiles.js";
 export { localforage };
 
+export { localforage };
 export {loadStats} from './src/js/get23_loadStats.js';
 export { displayProfiles } from './src/js/get23_loadProfiles.js';
 
